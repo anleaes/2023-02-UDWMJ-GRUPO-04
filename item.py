29489766083069328cc6f2a2ff2ba524657bf8f8
BrunoCP249 <brunochaves249@gmail.com>
2023-09-29 09:38:48 -0300

    item and order

---
+++ item.py
@@ -0,0 +1,6 @@
+class orderitem:
+def_init_(self,quantity,unitary_price,order: order,product: product):
+self.quantity = quantity
+self.unitary_price  
+self. order = order
+self. product = product
---
+++ order.py
@@ -0,0 +1,5 @@
+class order:
+def_init_(self,total_price,status,client):
+self.total_prices = total_price
+self.status = status
+self.client = client
