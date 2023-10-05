c0205bdcc0e0788a72ca23cf6135e18f1d33e1e2
BrunoCP249 <brunochaves249@gmail.com>
2023-10-05 11:12:52 -0300

    correção de erro

--- order.py
+++ order.py
@@ -1,6 +1,6 @@
 class order:
 
- def__init__(self ,total_price,status,client):
+ def __init__(self ,total_price,status,client):
   self.total_prices = total_price
   self.status = status
   self.client = client
