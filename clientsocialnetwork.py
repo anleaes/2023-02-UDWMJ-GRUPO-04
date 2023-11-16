14f04cd4b02a7bf0f4543ed37fedd396cb1ab26e
Gbarbosa05 <gabisilveirab@hotmail.com>
2023-11-16 09:08:26 -0300

    Projetoapp

--- clientsocialnetwork.py
+++
@@ -1,8 +0,0 @@
-class Clientsocialnetwork:
-    
-    def __init__(self, client, socialnetwork):
-        self.client = client
-        self.socialnetwork = socialnetwork 
-
-    def check_status(self):
-        print(f"Cliente: {self.first_name} {self.last_name}/ Gênero:{self.gender}/ Endereço:{self.address}/ Telefone:{self.cell_phone}/ Email:{self.email}.")
