b4329be9fbc00c17b16dcba2bf43a9b32e3a8393
MatheusDecarliGuarnieri <mdg2503@gmail.com>
2023-12-08 04:17:00 -0300

    criação das models do pagamento

--- apps/payment/models.py
+++ apps/payment/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    type_payment = models.CharField('Forma de Pagamento', max_length=50)
+    description = models.TextField('Descricao', max_length=100) 
+    status = models.BooleanField('Em Aberto', default=False)
+    
+    class Meta:
+        verbose_name = 'Pagamento'
+        verbose_name_plural = 'Pagamentos'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
