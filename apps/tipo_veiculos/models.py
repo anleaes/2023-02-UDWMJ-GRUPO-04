c028188679bef8e36be5e5847874d8d966721d09
Gbarbosa05 <gabisilveirab@hotmail.com>
2023-12-08 02:02:05 -0300

    criação models

--- apps/tipo_veiculos/models.py
+++ apps/tipo_veiculos/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class Category(models.Model):
+
+class TipoVeiculos(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     name = models.CharField('Nome', max_length=50)
     description = models.TextField('Descricao', max_length=100) 
     
     class Meta:
-        verbose_name = 'Categoria'
-        verbose_name_plural = 'Categorias'
+        verbose_name = 'Type_vehicles'
+        verbose_name_plural = 'Types_vehicles'
         ordering =['id']
 
     def __str__(self):
